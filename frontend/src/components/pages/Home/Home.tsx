38a4c13b6fa14dbf74454ba46f54b7b585cb31ed
ScuBix1 <bastian.monnin1@gmail.com>
2025-02-24 22:53:36 +0100

    feat(Home): add map and hover arrondissement

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,11 +1,18 @@
-import MapLyon from "../../MapLyon/MapLyon";
+import MapLyon from '../../MapLyon/MapLyon';
 
 const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-y-4">
       <h1 className="text-3xl">Les Cris de Lyon</h1>
       <div>
-        <MapLyon width={500} height={500} />
+        <MapLyon
+          width={500}
+          height={500}
+          fillColorNumber="fill-[var(--color-number)]"
+          fillColorArrondissement="fill-[var(--color-arrondissement)] cursor-pointer"
+          fillColorArrondissementHover="hover:fill-[var(--hover-color-arrondissement)]"
+          fillColorFleuve="fill-[var(--color-fleuve)]"
+        />
       </div>
     </div>
   );
